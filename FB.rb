a258464d86de821a82b7d975ace8aa9e0e882b5e
Bryn <bryn2009@live.co.uk>
2021-11-10 16:18:47 +0000

    working FizzBuzz

--- FB.rb
+++ FB.rb
@@ -1,23 +1,23 @@
 alphabet = ("a".."z").to_a
-input = gets.chomp
-while alphabet.include?(input) == false do
+
+while true do
+
+  input = gets.chomp
   
   if input == 'quit'
     break
   end
-  
-  input = input.to_i
-  if input % 3 == 0 && input % 5 == 0 
-    puts "FizzBuzz"
-    
-  elsif input % 5 == 0
-    puts "Buzz"
-    
-  elsif input % 3 == 0 
-    puts "Fizz"
-    
-  elsif
-    puts input
-    
+
+  if alphabet.include?(input[0]) == false
+    input = input.to_i
+    if input % 3 == 0 && input % 5 == 0 
+      puts "FizzBuzz"
+    elsif input % 5 == 0 
+      puts "Buzz"
+    elsif input % 3 == 0 
+      puts "Fizz"
+    elsif 
+      puts input  
+    end
   end
 end
