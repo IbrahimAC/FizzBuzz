19d9375a2a95628d953a421f741fc4567dcb3757
IbrahimAC <Ibrahimchowdhury.notwork@gmail.com>
2021-11-10 16:03:49 +0000

    Second variation of FizzBuzz code

--- FB.rb
+++ FB.rb
@@ -1,13 +1,23 @@
-while true do
-  input = gets.chomp
+alphabet = ("a".."z").to_a
+input = gets.chomp
+while alphabet.include?(input) == false do
+  
+  if input == 'quit'
+    break
+  end
+  
   input = input.to_i
   if input % 3 == 0 && input % 5 == 0 
     puts "FizzBuzz"
+    
   elsif input % 5 == 0
     puts "Buzz"
+    
   elsif input % 3 == 0 
     puts "Fizz"
+    
   elsif
     puts input
+    
   end
 end
