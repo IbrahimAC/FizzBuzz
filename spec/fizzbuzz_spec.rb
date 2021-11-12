a6750c32f8b28e419c2bf1ac50b8f104015164cb
IbrahimAC <Ibrahimchowdhury.notwork@gmail.com>
2021-11-12 14:50:37 +0000

    second pairing session - 'finished' rspec

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,16 +2,16 @@ require './lib/fizzbuzz'
 
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
-    expect (fizzbuzz(3)).to eq "fizz"
+    expect(fizzbuzz(3)).to eq 'Fizz'
   end
   it 'returns "buzz" when passed 5' do 
-    expect (fizzbuzz(5)).to eq 'buzz'
+    expect(fizzbuzz(5)).to eq 'Buzz'
   end
   it 'returns "fizzbuzz" when passed 15' do
-    expect (fizzbuzz(15)).to eq 'fizzbuzz'
+    expect(fizzbuzz(15)).to eq 'Fizzbuzz'
   end
   it 'returns "1" when passed 1' do
-    expect (fizzbuzz(1)).to eq '1'
+    expect(fizzbuzz(1)).to eq 1
   end
 end
  
